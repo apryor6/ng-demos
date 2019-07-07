c02529b3623069824650a9b69f549282129bdd85
apryor6 <apryor6@gmail.com>
2019-07-07 09:37:55 -0400

    Implement ripple effect

--- apps/jsripple/src/app/ripple.directive.ts
+++ apps/jsripple/src/app/ripple.directive.ts
@@ -8,14 +8,16 @@ export class RippleDirective {
   toggle(e: MouseEvent) {
     const node = document.createElement('div');
     node.className = 'circle zoom';
-    const x = e.offsetX - 25;
-    const y = e.offsetY - 25;
+    const offsetTop = this.el.nativeElement.offsetTop;
+    const offsetLeft = this.el.nativeElement.offsetLeft;
+    const x = e.pageX - offsetLeft - 25;
+    const y = e.pageY - offsetTop - 25;
     node.style.top = `${y}px`;
     node.style.left = `${x}px`;
     this.el.nativeElement.appendChild(node);
     setTimeout(() => {
       this.el.nativeElement.removeChild(node);
-    }, 4900);
+    }, 400);
   }
   constructor(private el: ElementRef) {}
 }
